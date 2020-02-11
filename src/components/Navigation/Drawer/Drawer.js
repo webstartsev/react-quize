52312ec34e5ecb232b0b4967cff0e37ec4433328
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-02-11 16:09:19 +0300

    import Backdrop component

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import classes from './Drawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const links = [1, 2, 3, 4];
 
@@ -21,9 +22,12 @@ class Drawer extends Component {
     }
 
     return (
-      <nav className={cls.join(` `)}>
-        <ul className={classes.Drawer_list}>{this.renderLinks(links)}</ul>
-      </nav>
+      <>
+        <nav className={cls.join(` `)}>
+          <ul className={classes.Drawer_list}>{this.renderLinks(links)}</ul>
+        </nav>
+        {this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null}
+      </>
     );
   }
 }
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -14,10 +14,16 @@ class Layout extends Component {
     });
   };
 
+  menuCloseHandler = () => {
+    this.setState({
+      isMenuOpen: false
+    });
+  };
+
   render() {
     return (
       <div className={classes.Layout}>
-        <Drawer isOpen={this.state.isMenuOpen} />
+        <Drawer isOpen={this.state.isMenuOpen} onClose={this.menuCloseHandler} />
         <MenuToggle onToggle={this.toggleMenuHandler} isOpen={this.state.isMenuOpen} />
         <main className={classes.LayoutMain}>{this.props.children}</main>
       </div>
