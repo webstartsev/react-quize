2f756f582a6b2f60646be0820d9a135f468abdd9
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-02-12 12:01:47 +0300

    update Auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import classes from './Auth.module.css';
+import Button from '../../components/UI/Button/Button';
 
 class Auth extends Component {
+  submitHandler = evt => {
+    evt.preventDefault();
+  };
+
+  loginHandler = () => {
+    console.log('loginHandler');
+  };
+
+  registerHandler = () => {
+    console.log('registerHandler');
+  };
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div className={classes.Auth_wrapper}>
+          <h1 className={classes.Auth_title}>Авторизация</h1>
+
+          <form onSubmit={this.submitHandler} className={classes.Auth_form}>
+            <input type="text" />
+            <input type="text" />
+
+            <Button type="success" onClick={this.loginHandler}>
+              Войти
+            </Button>
+            <Button type="primary" onClick={this.registerHandler}>
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
