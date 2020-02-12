9e86625a1ab9d8a5a8c429e474a26b493ea9d7f6
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-02-12 12:32:03 +0300

    implement Input

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './Auth.module.css';
 import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
 
 class Auth extends Component {
   submitHandler = evt => {
@@ -22,8 +23,8 @@ class Auth extends Component {
           <h1 className={classes.Auth_title}>Авторизация</h1>
 
           <form onSubmit={this.submitHandler} className={classes.Auth_form}>
-            <input type="text" />
-            <input type="text" />
+            <Input label="Email" />
+            <Input label="Пароль" type="password" errorMessage="Test" />
 
             <Button type="success" onClick={this.loginHandler}>
               Войти
