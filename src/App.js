8aaf744c3bb125b581de05c57bc13d0c42c535a3
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-02-12 10:51:42 +0300

    add Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import Layout from './hoc/Layout/Layout'
+import Layout from './hoc/Layout/Layout';
 import Quiz from './containers/Quiz/Quiz';
+import Auth from './containers/Auth/Auth';
+import QuizCreator from './containers/QuizCreator/QuizCreator';
+import QuizList from './containers/QuizList/QuizList';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Layout>
-      <Quiz />
+      <Switch>
+        <Route path="/auth" component={Auth}></Route>
+        <Route path="/quize-creator" component={QuizCreator}></Route>
+        <Route path="/quiz/:id" component={Quiz}></Route>
+        <Route path="/" component={QuizList}></Route>
+      </Switch>
     </Layout>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-const app = () => (
+const app = (
   <BrowserRouter>
     <App />
   </BrowserRouter>
