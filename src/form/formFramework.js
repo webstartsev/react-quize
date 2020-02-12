03d73da014cba4184622ca4b4d6f7ec98fc6964a
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-02-12 17:17:09 +0300

    add validateControl & validateForm functions

--- src/form/formFramework.js
+++ src/form/formFramework.js
@@ -1,3 +1,5 @@
+import is from 'is_js';
+
 export const createControl = (options, validation) => {
   return {
     ...options,
@@ -6,4 +8,38 @@ export const createControl = (options, validation) => {
     value: ``,
     touched: false
   };
+};
+
+export const validateControl = (value, validation) => {
+  if (!validation) {
+    return true;
+  }
+
+  let isValid = true;
+
+  if (validation.required) {
+    isValid = value.trim() !== `` && isValid;
+  }
+
+  if (validation.email) {
+    isValid = is.email(value) && isValid;
+  }
+
+  if (validation.minLength) {
+    isValid = value.length >= 6 && isValid;
+  }
+
+  return isValid;
+};
+
+export const validateForm = formControls => {
+  let isFormValid = true;
+
+  for (const control in formControls) {
+    if (formControls.hasOwnProperty(control)) {
+      isFormValid = formControls[control].valid && isFormValid;
+    }
+  }
+
+  return isFormValid;
 };
