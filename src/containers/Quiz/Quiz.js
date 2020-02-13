b255feb8f59c547a25821c534ba68c19260a8f62
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-02-13 11:13:09 +0300

    get Quiz from server

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -2,37 +2,17 @@ import React, { Component } from 'react';
 import classes from './Quiz.module.css';
 import ActiveQuize from '../../components/ActiveQuiz/ActiveQuiz';
 import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz';
+import axios from '../../axios/quiz';
+import Loader from '../../components/UI/Loader/Loader';
 
 class Quiz extends Component {
   state = {
+    loading: true,
     results: {},
     isFinished: false,
     activeQuestion: 0,
     answerState: null,
-    quiz: [
-      {
-        id: 1,
-        question: `Какого цвета небо?`,
-        rightAnswerId: 2,
-        answers: [
-          { text: `Черный`, id: 1 },
-          { text: `Синий`, id: 2 },
-          { text: `Красный`, id: 3 },
-          { text: `Зеленый`, id: 4 }
-        ]
-      },
-      {
-        id: 2,
-        question: `В каком году основа Санкт-Петербург`,
-        rightAnswerId: 3,
-        answers: [
-          { text: `1700`, id: 1 },
-          { text: `1705`, id: 2 },
-          { text: `1703`, id: 3 },
-          { text: `1803`, id: 4 }
-        ]
-      }
-    ]
+    quiz: []
   };
 
   onAnswerClickHandler = answerId => {
@@ -90,13 +70,30 @@ class Quiz extends Component {
     });
   };
 
+  componentDidMount = async () => {
+    try {
+      const quizeId = this.props.match.params.id;
+      const response = await axios.get(`/qiuzes/${quizeId}.json`);
+
+      const quiz = response.data;
+      this.setState({
+        quiz,
+        loading: false
+      });
+    } catch (err) {
+      console.log('err: ', err);
+    }
+  };
+
   render() {
     return (
       <div className={classes.Quiz}>
         <div className={classes.quizWrapper}>
           <h1 className={classes.Quiz_title}>Ответьте на все вопросы</h1>
 
-          {this.state.isFinished ? (
+          {this.state.loading ? (
+            <Loader />
+          ) : this.state.isFinished ? (
             <FinishedQuiz
               onRetry={this.retryHandler}
               results={this.state.results}
