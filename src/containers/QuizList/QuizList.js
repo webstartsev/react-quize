d0b3aaea5f2cf6094cbb4383e5db7cf8941a8c19
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-02-13 10:57:54 +0300

    get QuizeList from server

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import classes from './QuizList.module.css';
 import { NavLink } from 'react-router-dom';
+import Loader from '../../components/UI/Loader/Loader';
+import axios from 'axios';
 
 class QuizList extends Component {
+  state = {
+    quizes: [],
+    loading: true
+  };
+
   renderQuizeItem = () => {
-    return [1, 2, 3].map((item, i) => {
+    return this.state.quizes.map(quiz => {
       return (
-        <li key={i} className={classes.QuizList_item}>
-          <NavLink to={`/quiz/${item}`} className={classes.QuizList_link}>
-            Test {item}
+        <li key={quiz.id} className={classes.QuizList_item}>
+          <NavLink to={`/quiz/${quiz.id}`} className={classes.QuizList_link}>
+            {quiz.name}
           </NavLink>
         </li>
       );
     });
+  };
+
+  componentDidMount = async () => {
+    try {
+      const response = await axios.get(`https://react-quize-e85a5.firebaseio.com/qiuzes.json`);
+
+      const quizes = [];
+      Object.keys(response.data).forEach((key, i) => {
+        quizes.push({
+          id: key,
+          name: `Test #${i + 1}`
+        });
+      });
+
+      this.setState({
+        quizes,
+        loading: false
+      });
+    } catch (err) {
+      console.log('err: ', err);
+    }
   };
 
   render() {
@@ -20,8 +48,11 @@ class QuizList extends Component {
       <div className={classes.QuizList}>
         <div>
           <h1 className={classes.QuizList_title}>Quiz List</h1>
-
-          <ul className={classes.QuizList_list}>{this.renderQuizeItem()}</ul>
+          {this.state.loading ? (
+            <Loader />
+          ) : (
+            <ul className={classes.QuizList_list}>{this.renderQuizeItem()}</ul>
+          )}
         </div>
       </div>
     );
